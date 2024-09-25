528cc890f3b1cf5ce7c72fff18f0c5747c50043e
WallyDev <wallace07abreu@gmail.com>
2024-09-25 16:53:13 -0300

    feat:layout done

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,14 +1,23 @@
+"use client"
 
-import { NavSvg } from "../icons/navSvg";
+import { useState } from "react"
+
 
 
 export default function Nav ()
 {
 
+   const [open, setOpen] = useState<boolean>(false)
+//    onMouseOutCapture={}
     return(
         <nav>
-
-            <NavSvg></NavSvg>
+            <button onClick={() => setOpen(prev => !prev)}>            
+                {!open && <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25H12" />
+                </svg>}
+                {/* {open && <>X</>}<br></br>
+                {open && <>meninos</>} */}
+            </button>
         </nav>
     )
 
--- src/components/search.tsx
+++ src/components/search.tsx
@@ -22,7 +22,7 @@ export default function Search ()
                 <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
             </svg>
 
-            <input type="text" placeholder="Message" className="bg-gray-300 rounded-xl h-8 placeholder:text-gray-600 placeholder:p-4 flex"/>
+            <input type="text" placeholder="Message" className="bg-gray-300 rounded-xl h-8 placeholder:text-gray-600 placeholder:p-4 flex w-7/12 md:w-4/6"/>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-7">
                 <path strokeLinecap="round" strokeLinejoin="round" d="m15 11.25-3-3m0 0-3 3m3-3v7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
             </svg>
